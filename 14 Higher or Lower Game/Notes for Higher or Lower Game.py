e597e3fc7608324e5dc94a74969369c678ad2b51
CHRISTOPHERS\chris <christopherdillard99@gmail.com>
2024-05-13 22:50:41 -0400

    adding day 14 files for Higher or Lower game

---
+++ 14 Higher or Lower Game/Notes for Higher or Lower Game.py
@@ -0,0 +1,416 @@
+# Day 14: Higher or Lower Game (go to line 342):
+
+# Building a game called "Higher or Lower".
+# Consists in comparing one search term against another. 
+# You have to guess whether the first term has higher or lower searches.
+
+# If you guess wrong, you are then told how many you got right with a score and are asked if you want to play again.
+
+# The version of Higher or Lower that I will be creating has to do with comparing follower counts on Instagram.
+# The game will keep score after a correct guess, other the game ends and the user can play again.
+# after a correct guess the choice "A" is replaced with the choice "B" from the previous turn.
+# since the actually follow_count for is not revealed to the user, the random account can be chosen again in future rounds.
+# Choice A and B must not be the same randon account.
+
+# these are dictionaries that were provided for the logo art and the data in their respective files (art.py and game_data.py):
+
+# MY CODE STARTS AT LINE 342:
+
+logo = """
+    __  ___       __             
+   / / / (_)___ _/ /_  ___  _____
+  / /_/ / / __ `/ __ \/ _ \/ ___/
+ / __  / / /_/ / / / /  __/ /    
+/_/ ///_/\__, /_/ /_/\___/_/     
+   / /  /____/_      _____  _____
+  / /   / __ \ | /| / / _ \/ ___/
+ / /___/ /_/ / |/ |/ /  __/ /    
+/_____/\____/|__/|__/\___/_/     
+"""
+
+vs = """
+ _    __    
+| |  / /____
+| | / / ___/
+| |/ (__  ) 
+|___/____(_)
+"""
+
+data = [
+    {
+        'name': 'Instagram',
+        'follower_count': 346,
+        'description': 'Social media platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Cristiano Ronaldo',
+        'follower_count': 215,
+        'description': 'Footballer',
+        'country': 'Portugal'
+    },
+    {
+        'name': 'Ariana Grande',
+        'follower_count': 183,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Dwayne Johnson',
+        'follower_count': 181,
+        'description': 'Actor and professional wrestler',
+        'country': 'United States'
+    },
+    {
+        'name': 'Selena Gomez',
+        'follower_count': 174,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kylie Jenner',
+        'follower_count': 172,
+        'description': 'Reality TV personality and businesswoman and Self-Made Billionaire',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kim Kardashian',
+        'follower_count': 167,
+        'description': 'Reality TV personality and businesswoman',
+        'country': 'United States'
+    },
+    {
+        'name': 'Lionel Messi',
+        'follower_count': 149,
+        'description': 'Footballer',
+        'country': 'Argentina'
+    },
+    {
+        'name': 'Beyoncé',
+        'follower_count': 145,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Neymar',
+        'follower_count': 138,
+        'description': 'Footballer',
+        'country': 'Brasil'
+    },
+    {
+        'name': 'National Geographic',
+        'follower_count': 135,
+        'description': 'Magazine',
+        'country': 'United States'
+    },
+    {
+        'name': 'Justin Bieber',
+        'follower_count': 133,
+        'description': 'Musician',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Taylor Swift',
+        'follower_count': 131,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kendall Jenner',
+        'follower_count': 127,
+        'description': 'Reality TV personality and Model',
+        'country': 'United States'
+    },
+    {
+        'name': 'Jennifer Lopez',
+        'follower_count': 119,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Nicki Minaj',
+        'follower_count': 113,
+        'description': 'Musician',
+        'country': 'Trinidad and Tobago'
+    },
+    {
+        'name': 'Nike',
+        'follower_count': 109,
+        'description': 'Sportswear multinational',
+        'country': 'United States'
+    },
+    {
+        'name': 'Khloé Kardashian',
+        'follower_count': 108,
+        'description': 'Reality TV personality and businesswoman',
+        'country': 'United States'
+    },
+    {
+        'name': 'Miley Cyrus',
+        'follower_count': 107,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Katy Perry',
+        'follower_count': 94,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kourtney Kardashian',
+        'follower_count': 90,
+        'description': 'Reality TV personality',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kevin Hart',
+        'follower_count': 89,
+        'description': 'Comedian and actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'Ellen DeGeneres',
+        'follower_count': 87,
+        'description': 'Comedian',
+        'country': 'United States'
+    },
+    {
+        'name': 'Real Madrid CF',
+        'follower_count': 86,
+        'description': 'Football club',
+        'country': 'Spain'
+    },
+    {
+        'name': 'FC Barcelona',
+        'follower_count': 85,
+        'description': 'Football club',
+        'country': 'Spain'
+    },
+    {
+        'name': 'Rihanna',
+        'follower_count': 81,
+        'description': 'Musician and businesswoman',
+        'country': 'Barbados'
+    },
+    {
+        'name': 'Demi Lovato',
+        'follower_count': 80,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': "Victoria's Secret",
+        'follower_count': 69,
+        'description': 'Lingerie brand',
+        'country': 'United States'
+    },
+    {
+        'name': 'Zendaya',
+        'follower_count': 68,
+        'description': 'Actress and musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Shakira',
+        'follower_count': 66,
+        'description': 'Musician',
+        'country': 'Colombia'
+    },
+    {
+        'name': 'Drake',
+        'follower_count': 65,
+        'description': 'Musician',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Chris Brown',
+        'follower_count': 64,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'LeBron James',
+        'follower_count': 63,
+        'description': 'Basketball player',
+        'country': 'United States'
+    },
+    {
+        'name': 'Vin Diesel',
+        'follower_count': 62,
+        'description': 'Actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'Cardi B',
+        'follower_count': 67,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'David Beckham',
+        'follower_count': 82,
+        'description': 'Footballer',
+        'country': 'United Kingdom'
+    },
+    {
+        'name': 'Billie Eilish',
+        'follower_count': 61,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Justin Timberlake',
+        'follower_count': 59,
+        'description': 'Musician and actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'UEFA Champions League',
+        'follower_count': 58,
+        'description': 'Club football competition',
+        'country': 'Europe'
+    },
+    {
+        'name': 'NASA',
+        'follower_count': 56,
+        'description': 'Space agency',
+        'country': 'United States'
+    },
+    {
+        'name': 'Emma Watson',
+        'follower_count': 56,
+        'description': 'Actress',
+        'country': 'United Kingdom'
+    },
+    {
+        'name': 'Shawn Mendes',
+        'follower_count': 57,
+        'description': 'Musician',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Virat Kohli',
+        'follower_count': 55,
+        'description': 'Cricketer',
+        'country': 'India'
+    },
+    {
+        'name': 'Gigi Hadid',
+        'follower_count': 54,
+        'description': 'Model',
+        'country': 'United States'
+    },
+    {
+        'name': 'Priyanka Chopra Jonas',
+        'follower_count': 53,
+        'description': 'Actress and musician',
+        'country': 'India'
+    },
+    {
+        'name': '9GAG',
+        'follower_count': 52,
+        'description': 'Social media platform',
+        'country': 'China'
+    },
+    {
+        'name': 'Ronaldinho',
+        'follower_count': 51,
+        'description': 'Footballer',
+        'country': 'Brasil'
+    },
+    {
+        'name': 'Maluma',
+        'follower_count': 50,
+        'description': 'Musician',
+        'country': 'Colombia'
+    },
+    {
+        'name': 'Camila Cabello',
+        'follower_count': 49,
+        'description': 'Musician',
+        'country': 'Cuba'
+    },
+    {
+        'name': 'NBA',
+        'follower_count': 47,
+        'description': 'Club Basketball Competition',
+        'country': 'United States'
+    }
+]
+
+# Starting below is the code to create and ensure that user experience is "up to code" :p 
+
+# import the modules and data needed for this project, along with ASCII art contained in files (same info posted above these lines):
+
+import random
+
+from replit import clear # this will be used to refresh the game if user guesses wrong and wants another try.
+from art import logo, vs # use comma separator to import multiple variables from the art file.
+from game_data import data # data is the list of dictionaries in the game_data file, containing account name, follower_count, description, and country as keys. 
+
+# these are the functions that are critical to the games playability with explanations in the '''doc strings''':
+
+def load_random_account():
+  """Retrieves data from a random account in the 'data' list of dictionaries."""
+  return random.choice(data)
+
+def format_data(account):
+  """Reformats accounts into a printable format with account name, description and country"""
+  name = account["name"]
+  description = account["description"]
+  country = account["country"]
+  # print(f'{name}: {account["follower_count"]}') could be used to reveal this information to the user upon a right guess, but that's too easy...
+  return f"{name}, a {description}, from {country}"
+
+def check_answer(guess, a_followers, b_followers):
+  """Checks recorded value for follower count against user's guess 
+  and returns True if they guessed correctly.
+  Otherwise, False if they got it wrong.""" 
+  if a_followers > b_followers:
+    return guess == "a"
+  else:
+    return guess == "b"
+
+
+def load_game():
+  print(logo)
+  score = 0 # later, a correct guess will add 1 to this default 0 score, otherwise the game ends. 
+  continue_game = True
+  account_a = load_random_account()
+  account_b = load_random_account()
+
+  while continue_game: #the colon placement here without a boolean or comparison operator in this while statement implicitly checks for a True condition for continue_game.
+    account_a = account_b # in the next round, the account b replaces account a - this avoids a situation where a_random_account is instagram, which will always have more followers than anyone else which is no fun.
+    account_b = load_random_account()
+
+    while account_a == account_b: # this ensures that the next random account for b is not the same insta account as a.
+      account_b = load_random_account()
+
+    print(f"Compare A: {format_data(account_a)}.") # this prompt is standard with each round to ensure consistency, so correct indentation matters while nested here.
+    print(vs)
+    print(f"Against B: {format_data(account_b)}.")
+    print("---------------------------------------------") # this is really just added to add some space and help with readability in the console for the user.
+    
+    user_guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(user_guess, a_follower_count, b_follower_count)
+
+    clear() # this way the console has a clean feel and new rounds down tag along after old ones... avoiding clutter.
+    print(logo)
+    if is_correct:
+      score += 1
+      print(f"Correctamundo! Current score: {score}.")
+    else:
+      game_should_continue = False
+      print(f"Better luck next time! Final score: {score}")
+      play_again = input("Play again? 'y' or 'n': ").lower()
+      if play_again == "y":
+        clear()
+        load_game()
+
+load_game()
+
+
+
